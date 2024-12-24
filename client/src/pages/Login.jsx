f934250b1fd3f632891d6cb4b038622b16fef166
Aariz-Siddiqui <arizsiddiqui93@gmail.com>
2024-12-24 18:58:41 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(user); 
-    const response = await fetch("http://localhost:8000/login", {
+    const response = await fetch("https://gbim-vercel.vercel.app/login", {
         method:"POST",
         headers:{
             "Content-Type":"application/json"
