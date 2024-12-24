8d8b25e2b9cbc71caa0a4b49f12fca72ddd5905b
Aariz-Siddiqui <arizsiddiqui93@gmail.com>
2024-12-24 20:09:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,19 +4,14 @@ const router = require("./router");
 const connectdb = require("./database/db");
 const cors = require("cors");
 const corsOption ={
-    origin:"https://gbim-vercel-zkwo.vercel.app",
+    origin:["https://gbim-vercel-zkwo.vercel.app"],
     methods:"GET,POST,PUT,PATCH,DELETE,HEAD",
     credentials:true,
-     allowedHeaders: "Content-Type,Authorization",
-
 };
 app.use(cors(corsOption));
 app.use(express.json());
 app.use("/",router);
 app.get("/", (req,res)=>{res.send("Welcome to the backend")});
-app.options("/register", cors(corsOptions));
-app.options("/login", cors(corsOptions));
-app.options("/contact", cors(corsOptions));
 router.route("/register");
 router.route("/login");
 router.route("/contact");
