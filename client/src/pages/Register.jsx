2609e855d4292f6485fe65565497a6e525dd8582
Aariz-Siddiqui <arizsiddiqui93@gmail.com>
2024-12-25 18:07:01 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -24,7 +24,6 @@ const Register = () => {
     console.log(user); 
     const response = await fetch("https://gbim-vercel-backend.vercel.app/register", {
         method:"POST",
-        mode: 'no-cors',
         headers:{
             "Content-Type":"application/json"
         },
