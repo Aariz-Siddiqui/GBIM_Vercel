79db2630affa9f1f92bccc100ca9d117e818285f
Aariz-Siddiqui <arizsiddiqui93@gmail.com>
2024-12-25 17:56:08 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
+router.route("/").get(controller.home);
 router.route("/register").post(controller.register);
 router.route("/login").post(controller.login);
 router.route("/contact").post(controller.contact);
