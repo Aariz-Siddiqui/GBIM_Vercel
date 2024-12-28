cf803471e6f4c760b723cc1ade5188f6b8a08590
Aariz-Siddiqui <arizsiddiqui93@gmail.com>
2024-12-28 14:50:08 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./App.css"
-import "./contact.css"
-import "./register.css"
-import "./login.css"
+//import "./App.css"
+//import "./contact.css"
+//import "./register.css"
+//import "./login.css"
 import { Header } from "./Components/Header";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import './navbar.css'; // You can customize the styles in this file
+//import './navbar.css'; // You can customize the styles in this file
 import Contact from "./pages/Contact";
 import Feautres from "./pages/Feautres";
 import Pricing from "./pages/Pricing";
